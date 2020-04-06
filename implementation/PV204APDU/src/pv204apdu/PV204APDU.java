909aa639754841e19bd440fd4be12e9019dffb52
NimRo97 <nimronagy@gmail.com>
2020-04-06 12:21:58 +0200

    Cleaned the demo for authenticated ECDH implementation

--- implementation/PV204APDU/src/pv204apdu/PV204APDU.java
+++ implementation/PV204APDU/src/pv204apdu/PV204APDU.java
@@ -14,173 +14,162 @@ import java.security.spec.ECGenParameterSpec;
 import java.security.spec.ECParameterSpec;
 import java.security.spec.ECPoint;
 import java.security.spec.ECPublicKeySpec;
-import java.security.spec.EllipticCurve;
 import javax.crypto.KeyAgreement;
 
 import javax.smartcardio.CommandAPDU;
 import javax.smartcardio.ResponseAPDU;
 
 /**
- * Test class.
- * Note: If simulator cannot be started try adding "-noverify" JVM parameter
- *
- * @author Petr Svenda (petrs), Dusan Klinec (ph4r05)
+ * 
+ * @author Imrich Nagy
+ * Based on work of Petr Svenda (petrs), Dusan Klinec (ph4r05)
  */
 public class PV204APDU {
-    private static String APPLET_AID = "482871d58ab7465e5e05";
-    private static byte APPLET_AID_BYTE[] = Util.hexStringToByteArray(APPLET_AID);
-
-    private static final String STR_APDU_GETRANDOM = "B054100000";
-    private static final String STR_APDU_VERIFYPIN = "B05500000401020304";
-
-    /**
-     * Main entry point.
-     *
-     * @param args
-     */
+    private static final String APPLET_AID = "482871d58ab7465e5e05";
+    private static final byte APPLET_AID_BYTE[] = Util.hexStringToByteArray(APPLET_AID);
+    
+    private static final String PIN_LENGTH = "04";
+    
+    final CardManager cardMngr = new CardManager(true, APPLET_AID_BYTE);
+    byte[] ecdhSecret = null;
+    byte[] pinSecret = null;
+    
     public static void main(String[] args) {
         try {
             PV204APDU main = new PV204APDU();
             
-            //main.demoGetRandomDataCommand();
-            //main.demoInstallPIN();
-            main.demoGenerateECDH();
+            main.installPinAndConnect();
+            main.startEcdhSession();
             
+            System.out.println("\nControl of the correctness of the PIN and ECDH Secret:");
+            main.compareSecretWithCard();
+            main.comparePinWithCard();
+                    
         } catch (Exception ex) {
-            System.out.println("Exception : " + ex);
+            System.out.println("Exception: " + ex);
         }
     }
     
-    public void demoGenerateECDH() throws Exception {
-        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("EC");
-        keyGen.initialize(224);
-        KeyPair keyPair = keyGen.generateKeyPair();
+    public void installPinAndConnect() throws Exception {
+        final RunConfig runCfg = RunConfig.getDefaultConfig();
+        runCfg.setAppletToSimulate(PV204Applet.class);
+        runCfg.setTestCardType(RunConfig.CARD_TYPE.JCARDSIMLOCAL);
         
+        byte[] installCommand = Util.hexStringToByteArray("0a" + APPLET_AID + "00" + PIN_LENGTH);
+        byte[] pin = {(byte) 0x01, (byte) 0x02, (byte) 0x03, (byte) 0x04};
+        
+        byte[] installData = Util.concat(installCommand, pin);
+        runCfg.setInstallData(installData);
+        
+        System.out.print("Connecting to card...");
+        if (!cardMngr.Connect(runCfg)) {
+            System.out.println(" Failed.");
+        }
+        System.out.println(" Done.");
+        
+        pinSecret = pin;
+    }
+    
+    public void startEcdhSession() throws Exception {
+
         KeyAgreement dh = KeyAgreement.getInstance("ECDH");
+        
+        //These are the bytes to send to card
+        byte[] pcEcdhShare = prepareKeyPair(dh);
+        
+        //send PC ECDH share and receive card ECDH share
+        byte[] cardEcdhShare = exchangeEcdhShares(pcEcdhShare);
+        
+        ECPublicKey cardPublicKey = extractCardPublicKey(cardEcdhShare);
+        dh.doPhase(cardPublicKey, true);
+        byte[] derivedSecret = dh.generateSecret();
+        
+        MessageDigest md = MessageDigest.getInstance("SHA");
+        ecdhSecret = md.digest(derivedSecret);
+        
+        deriveSessionKey();
+    }
+    
+    //derive session key from shared ECDH secret
+    private void deriveSessionKey() {
+        //TODO
+    }
+    
+    //for debugging
+    public void compareSecretWithCard() throws Exception {
+        
+        //get shared ECDH secret from card
+        final ResponseAPDU response = cardMngr.transmit(new CommandAPDU(Util.hexStringToByteArray("B0600000")));
+        byte[] cardEcdhSecret = response.getData();
+        
+        System.out.println("PC   ECDH secret: " + Util.bytesToHex(ecdhSecret));
+        System.out.println("Card ECDH secret: " + Util.bytesToHex(cardEcdhSecret));
+    }
+    
+    //for debugging
+    public void comparePinWithCard() throws Exception {
+        
+        //get PIN from card
+        final ResponseAPDU response = cardMngr.transmit(new CommandAPDU(Util.hexStringToByteArray("B0610000")));
+        byte[] cardPin = response.getData();
+        
+        System.out.println("PC   PIN: " + Util.bytesToHex(pinSecret));
+        System.out.println("Card PIN: " + Util.bytesToHex(cardPin));
+    }
+    
+    public byte[] exchangeEcdhShares(byte[] pcEcdhShare) throws Exception {
+        byte[] command = {(byte) 0xb0, (byte) 0x59, (byte) 0x00, (byte) 0x00,
+                          (byte) pcEcdhShare.length};
+        byte[] sendData = Util.concat(command, pcEcdhShare);
+        
+        final ResponseAPDU response = cardMngr.transmit(new CommandAPDU(sendData));
+        return response.getData();
+    }
+    
+    public byte[] prepareKeyPair(KeyAgreement dh) throws Exception {
+        KeyPair keyPair = getRandomEcKeyPair();
+        
         dh.init(keyPair.getPrivate());
         
-        ECPoint w = ((ECPublicKey) keyPair.getPublic()).getW();
+        return encodeEcPublicKey((ECPublicKey) keyPair.getPublic());
+    }
+    
+    public byte[] encodeEcPublicKey(ECPublicKey key) {
+        ECPoint w = key.getW();
         byte[] affineX = w.getAffineX().toByteArray();
         byte[] affineY = w.getAffineY().toByteArray();
         int keyLen = 224/8 + 1;
-        //if (affineX.length)
         
         byte[] x962encoded = new byte[2 * keyLen + 1];
         x962encoded[0] = (byte) 0x04;
         System.arraycopy(affineX, 0, x962encoded, 1 + keyLen - affineX.length, affineX.length);
         System.arraycopy(affineY, 0, x962encoded, 1 + 2* keyLen - affineY.length, affineY.length);
         
-        byte[] data = Util.concat(Util.hexStringToByteArray("B05900003B"), x962encoded);
-        System.out.println(Util.bytesToHex(data));
-        
-        
-        final CardManager cardMngr = new CardManager(true, APPLET_AID_BYTE);
-        final RunConfig runCfg = RunConfig.getDefaultConfig();
-        runCfg.setAppletToSimulate(PV204Applet.class);
-        runCfg.setTestCardType(RunConfig.CARD_TYPE.JCARDSIMLOCAL);
-        
-        byte[] installData = Util.hexStringToByteArray("0a" + APPLET_AID +
-                                                       "00" +
-                                                       "0401020304");
-        runCfg.setInstallData(installData);
-        
-        System.out.print("Connecting to card...");
-        if (!cardMngr.Connect(runCfg)) {
-            System.out.println(" Failed.");
-        }
-        System.out.println(" Done.");
-        
-        final ResponseAPDU response = cardMngr.transmit(new CommandAPDU(data));
-        byte[] responseData = response.getData();
-        final ResponseAPDU response2 = cardMngr.transmit(new CommandAPDU(Util.hexStringToByteArray("B0600000")));
-        byte[] secret = response2.getData();
-        
+        return x962encoded;
+    }
+    
+    ECPublicKey extractCardPublicKey(byte[] cardEcdhShare) throws Exception {
         byte[] cardX = new byte[28];
         byte[] cardY = new byte[28];
         
-        System.arraycopy(responseData, 1, cardX, 0, 28);
-        System.arraycopy(responseData, 29, cardY, 0, 28);
+        System.arraycopy(cardEcdhShare, 1, cardX, 0, 28);
+        System.arraycopy(cardEcdhShare, 29, cardY, 0, 28);
         
-        // generate bogus keypair(!) with named-curve params
-        KeyPairGenerator kpg = KeyPairGenerator.getInstance("EC");
-        ECGenParameterSpec gps = new ECGenParameterSpec ("secp224r1"); // NIST P-256 
-        kpg.initialize(gps);
-        KeyPair apair = kpg.generateKeyPair();
-        ECPublicKey apub  = (ECPublicKey)apair.getPublic();
-        ECParameterSpec aspec = apub.getParams();
-        // could serialize aspec for later use (in compatible JRE)
-        //
-        // for test only reuse bogus pubkey, for real substitute values 
-        ECPoint apoint = apub.getW();
         BigInteger x = new BigInteger(cardX);
         BigInteger y = new BigInteger(cardY);
-        // construct point plus params to pubkey
-        ECPoint bpoint = new ECPoint (x,y);
-        ECPublicKeySpec bpubs = new ECPublicKeySpec (bpoint, aspec);
-        KeyFactory kfa = KeyFactory.getInstance ("EC");
-        ECPublicKey bpub = (ECPublicKey) kfa.generatePublic(bpubs);
         
-        dh.doPhase(bpub, true);
-        byte[] derivedSecret = dh.generateSecret();
+        ECPoint ecPoint = new ECPoint (x,y);
+
+        ECParameterSpec ecSpec = ((ECPublicKey) getRandomEcKeyPair().getPublic()).getParams();
+        ECPublicKeySpec ecPkeySpec = new ECPublicKeySpec (ecPoint, ecSpec);
         
-        MessageDigest md = MessageDigest.getInstance("SHA");
-        byte[] hash = md.digest(derivedSecret);
-        
-        System.out.println(Util.bytesToHex(hash));
-        
+        return (ECPublicKey) KeyFactory.getInstance("EC").generatePublic(ecPkeySpec);
     }
     
-    public void demoInstallPIN() throws Exception {
-        
-        final CardManager cardMngr = new CardManager(true, APPLET_AID_BYTE);
-        final RunConfig runCfg = RunConfig.getDefaultConfig();
-        runCfg.setAppletToSimulate(PV204Applet.class);
-        runCfg.setTestCardType(RunConfig.CARD_TYPE.JCARDSIMLOCAL);
-        
-        byte[] installData = Util.hexStringToByteArray("0a" + APPLET_AID +
-                                                       "00" +
-                                                       "0401020304");
-        runCfg.setInstallData(installData);
-        
-        System.out.print("Connecting to card...");
-        if (!cardMngr.Connect(runCfg)) {
-            System.out.println(" Failed.");
-        }
-        System.out.println(" Done.");
-        
-        final ResponseAPDU response = cardMngr.transmit(new CommandAPDU(Util.hexStringToByteArray(STR_APDU_VERIFYPIN)));
-        byte[] data = response.getData();
-        
+    public KeyPair getRandomEcKeyPair() throws Exception {
+        KeyPairGenerator keyGen = KeyPairGenerator.getInstance("EC");
+        keyGen.initialize(new ECGenParameterSpec ("secp224r1"));
+        return keyGen.generateKeyPair();
     }
-
-    public void demoGetRandomDataCommand() throws Exception {
-        // CardManager abstracts from real or simulated card, provide with applet AID
-        final CardManager cardMngr = new CardManager(true, APPLET_AID_BYTE);          
-        
-        // Get default configuration for subsequent connection to card (personalized later)
-        final RunConfig runCfg = RunConfig.getDefaultConfig();
-
-        // A) If running on physical card
-        // runCfg.setTestCardType(RunConfig.CARD_TYPE.PHYSICAL); // Use real card
-
-        // B) If running in the simulator 
-        runCfg.setAppletToSimulate(PV204Applet.class); // main class of applet to simulate
-        runCfg.setTestCardType(RunConfig.CARD_TYPE.JCARDSIMLOCAL); // Use local simulator
-
-        // Connect to first available card
-        // NOTE: selects target applet based on AID specified in CardManager constructor
-        System.out.print("Connecting to card...");
-        if (!cardMngr.Connect(runCfg)) {
-            System.out.println(" Failed.");
-        }
-        System.out.println(" Done.");
-
-        // Transmit single APDU
-        final ResponseAPDU response = cardMngr.transmit(new CommandAPDU(Util.hexStringToByteArray(STR_APDU_GETRANDOM)));
-        byte[] data = response.getData();
-        
-        final ResponseAPDU response2 = cardMngr.transmit(new CommandAPDU(0xB0, 0x54, 0x00, 0x00, data)); // Use other constructor for CommandAPDU
-        
-        System.out.println(response);
-    }    
+    
 }
